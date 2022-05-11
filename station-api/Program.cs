6ccea385dfb00233862aaedb76f35e21ec9a8fae
K.Miya <51694607+k-miya0617@users.noreply.github.com>
2022-05-11 23:28:26 +0900

    cors設定追加

--- station-api/Program.cs
+++ station-api/Program.cs
@@ -1,5 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Corsオブジェクトを作成する
+var AllowLocalhost = "_allowLocalhost";
+var AllowDeployServer = "_allowDeployServer";
+
+// Corsの詳細を定義する
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowLocalhost,
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:13125")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+    options.AddPolicy(name: AllowDeployServer,
+        builder =>
+        {
+            builder.WithOrigins("http://192.168.1.125:13125")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -15,6 +38,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Corsを適用させる
+app.UseCors(AllowLocalhost);
+app.UseCors(AllowDeployServer);
 
 app.UseAuthorization();
 
